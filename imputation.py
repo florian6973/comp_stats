c2a977b1cb0b58f97fa782ec963bb1d739a973a8
florian6973 <ap.ap@club-internet.fr>
2024-01-05 00:47:42 +0100

    sweep

--- imputation.py
+++ imputation.py
@@ -1,4 +1,5 @@
 import lightning.pytorch as L
+from tqdm import tqdm
 import cs.model as M
 import yaml
 import cs.dataset as D
@@ -7,16 +8,17 @@ import numpy as np
 import torch
 import torch.distributions as td
 from torchvision.datasets import MNIST
+import os
 
 repertory = r"outputs\mnist_elbo_unconstrained_bernouilli_2023-12-31\mnist_elbo_unconstrained_bernouilli_2023-12-31\13-16-50"
 # repertory = r"outputs/mnist_elbo_unconstrained_bernouilli_2023-12-31/17-38-28"
 # repertory = r"outputs/mnist_elbo_unconstrained_bernouilli_2023-12-31/17-45-52"
-repertory = r"outputs/mnist_elbo_unconstrained_bernouilli_2023-12-31/17-56-53"
+# repertory = r"outputs/mnist_elbo_unconstrained_bernouilli_2023-12-31/17-56-53"
 # file = r"\lightning_logs\version_0\checkpoints\epoch=199-step=35400.ckpt"
-# file = r"\lightning_logs\version_0\checkpoints\epoch=1999-step=54000.ckpt"
+file = r"\lightning_logs\version_0\checkpoints\epoch=1999-step=54000.ckpt"
 # file = r"\lightning_logs\version_0\checkpoints\epoch=49-step=1350.ckpt"
 # file = r"\lightning_logs\version_0\checkpoints\epoch=19-step=540.ckpt"
-file = r"\lightning_logs\version_0\checkpoints\epoch=4-step=135.ckpt"
+# file = r"\lightning_logs\version_0\checkpoints\epoch=4-step=135.ckpt"
 
 
 # read cpkt
@@ -28,7 +30,7 @@ model = M.VAE.load_from_checkpoint(repertory + file, config=config, dim_input=di
 print(model.config)
 
 # test
-device = "cpu"
+device = "cuda" #"cpu"
 
 model.eval()
 
@@ -36,6 +38,12 @@ data = next(iter(test_loader))
 print(data.shape)
 
 dataset = MNIST("mnist", download=True)
+y_train = dataset.targets
+print(y_train.shape)
+print(torch.unique(y_train))
+idx_labels = {}
+for i in range(10):
+    idx_labels[i] = torch.where(y_train == i)[0]
 data = dataset.train_data.float().reshape(-1, 28*28)
 data = (data > 255/2).float()
 # data = next(iter(torch.utils.data.DataLoader(
@@ -116,7 +124,7 @@ def psmwg(model, img, mask, n_iter):
     scores = []
     acceptances = 0
     acceptances_rates = []
-    warmup = -1
+    warmup = 20 #-1
     img_miss = img * mask
     former_z = None
     prior = td.MultivariateNormal(torch.zeros(50).to(device), torch.eye(50).to(device))
@@ -170,60 +178,101 @@ def psmwg(model, img, mask, n_iter):
 with torch.no_grad():
     model = model.to(device)
 
-    for i, data_img in enumerate(data):
-        params = model.encoder(data_img)[:50]
-        print(params.shape)
-        log_px = model.decoder(params)
+    iterations = 10000
+    num_samples_per_classes = 8
+    stats_per_classes = {}
+    for label, idxes in idx_labels.items():
+        data_filtered = data[idxes]
+        print("\nLabel", label)
+        stats_per_classes[label] = {"gibbs": [], "mhwg": []}
+        for i, data_img in tqdm(enumerate(data_filtered[:num_samples_per_classes])):
+            data_img = data_img.to(device)
+            params = model.encoder(data_img)[:50]
+            print(params.shape)
+            log_px = model.decoder(params)
 
-        # plot(log_px, data_img)
+            # plot(log_px, data_img)
 
-        # mask = torch.Tensor(np.random.binomial(1, 0.5, (1, 784))).to(device)
-        mask = torch.zeros_like(data_img).reshape(28,28)
-        mask[28//2:, :] = 1.0
-        mask = mask.reshape(1, 784).to(device)
+            # mask = torch.Tensor(np.random.binomial(1, 0.5, (1, 784))).to(device)
+            mask = torch.zeros_like(data_img).reshape(28,28)
+            mask[28//2:, :] = 1.0
+            mask = mask.reshape(1, 784).to(device)
 
-        # img_miss = data_img * mask
-        # plot(log_px, img_miss)
+            # img_miss = data_img * mask
+            # plot(log_px, img_miss)
 
-        iterations = 2000
-        img_miss, scores = psgibbs(model, data_img, mask, iterations)
-        # plot_out(img_miss, data_img)
+            img_miss, scores = psgibbs(model, data_img, mask, iterations)
+            stats_per_classes[label]["gibbs"].append(scores[-1])
+            # plot_out(img_miss, data_img)
 
-        img_miss2, scores2, acceptances_rate = psmwg(model, data_img, mask, iterations)
-        # plot_out(img_miss, data_img)
+            img_miss2, scores2, acceptances_rate = psmwg(model, data_img, mask, iterations)
+            stats_per_classes[label]["mhwg"].append(scores2[-1])
+            # plot_out(img_miss, data_img)
 
-        # plot_out(img_miss, data_img, img_miss2)
+            # plot_out(img_miss, data_img, img_miss2)
 
-        plt.figure(figsize=(10, 10))
-        plt.subplot(4,1, 1)
-        plt.plot(scores, label="Gibbs")
-        plt.plot(scores2, label="MWG")
-        plt.legend()
-        plt.xlabel("Iteration")
-        plt.ylabel("F1 score")
-        plt.subplot(4,1, 2)
-        plt.imshow((data_img.reshape(28, 28).cpu().numpy()), cmap='gray')
-        plt.title("Original")
-        plt.axis('off')
-        plt.subplot(4,1, 3)
-        plt.imshow((img_miss.reshape(28, 28).cpu().numpy()), cmap='gray')
-        plt.axis('off')
-        plt.title("Gibbs")
-        plt.subplot(4,1, 4)
-        plt.imshow((img_miss2.reshape(28, 28).cpu().numpy()), cmap='gray')
-        plt.axis('off')
-        plt.title("MWG")
-        plt.tight_layout()
-        plt.savefig(f"sample-prob-e-{i}.png")
-        plt.close('all')
+            folder = f"samples/mnist/{label}/{i}/"
+            os.makedirs(folder, exist_ok=True)
+            # plt.figure(figsize=(10, 10))
+            fig, axes = plt.subplot_mosaic("AAAA;BCDE",figsize=(12, 8))
+            axes = list(axes.values())
+            # plt.subplot(4,1, 1)
+            axes[0].plot(scores, label="Gibbs")
+            axes[0].plot(scores2, label="MWG")
+            axes[0].set_title("F1-Score")
+            axes[0].grid()
+            axes[0].set_ylim(0.5, 1)
+            axes[0].legend()
+            axes[0].set_xlabel("Iteration")
+            axes[0].set_ylabel("F1 score")
+            # plt.subplot(4,1, 2)
+            axes[1].imshow((data_img.reshape(28, 28).cpu().numpy()), cmap='gray')
+            axes[1].set_title("Original")
+            axes[1].axis('off')
+            
+            axes[2].imshow(((data_img * mask).reshape(28, 28).cpu().numpy()), cmap='gray')
+            axes[2].set_title("Imputed")
+            axes[2].axis('off')
+            # plt.subplot(4,1, 3)
+            axes[3].imshow((img_miss.reshape(28, 28).cpu().numpy()), cmap='gray')
+            axes[3].axis('off')
+            axes[3].set_title(f"Gibbs: F1-Score {scores[-1]:.2f}")
+            # plt.subplot(4,1, 4)
+            axes[4].imshow((img_miss2.reshape(28, 28).cpu().numpy()), cmap='gray')
+            axes[4].axis('off')
+            axes[4].set_title(f"MHWG: F1-Score {scores2[-1]:.2f}")
+            plt.tight_layout()
+            plt.savefig(f"{folder}mc-{label}-{i}.png")
 
-        plt.figure(figsize=(10, 10))
-        plt.plot(acceptances_rate)
-        plt.xlabel("Iteration")
-        plt.ylabel("Acceptance rate")
-        plt.tight_layout()
-        plt.savefig(f"acceptance-rate-e-{i}.png")
-        plt.close('all')
+            # plt.savefig(f"{folder}sample-prob-e-{i}.png")
+            plt.close('all')
+
+            plt.figure(figsize=(10, 10))
+            plt.plot(acceptances_rate)
+            plt.xlabel("Iteration")
+            plt.ylabel("Acceptance rate")
+            plt.tight_layout()
+            plt.savefig(f"{folder}acceptance-rate-mhwg-{label}-{i}.png")
+            plt.close('all')
+
+            np.savez(f"{folder}/data-{label}-{i}.npz",
+                     data_img=data_img.cpu().numpy(),
+                     img_imputed=(data_img * mask).cpu().numpy(),
+                     img_miss=img_miss.cpu().numpy(),
+                     img_miss2=img_miss2.cpu().numpy(),
+                     scores=scores,
+                     scores2=scores2,
+                     acceptances_rate=acceptances_rate)
+            
+    for label, stats in stats_per_classes.items():
+        print(label)
+        print("Gibbs", np.mean(stats["gibbs"]))
+        print("MH Gibbs", np.mean(stats["mhwg"]))
+
+    import json
+    with open(f"samples/mnist/stats.json", "w") as f:
+        json.dump(stats_per_classes, f)
+    
 
     exit()
 
